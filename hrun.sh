c6a3d74572c9bb6bb57f82b923e9e1d404eb00ca
hiteshvvr <hitesh.vvr@gmail.com>
2018-12-30 15:05:46 +0200

    modified midas2root.cxx for csv

--- hrun.sh
+++ hrun.sh
@@ -1,6 +1,27 @@
 clear
 #make clean
-make
+#make
 #clear
-./midas2root.exe ../run00001.mid
+./midas2root.exe ~/AllData/both/run00127.mid
+./midas2root.exe ~/AllData/both/run00129.mid
+./midas2root.exe ~/AllData/both/run00130.mid
+./midas2root.exe ~/AllData/both/run00131.mid
+./midas2root.exe ~/AllData/both/run00132.mid
+./midas2root.exe ~/AllData/both/run00133.mid
+./midas2root.exe ~/AllData/both/run00134.mid
+./midas2root.exe ~/AllData/both/run00135.mid
+./midas2root.exe ~/AllData/both/run00136.mid
+./midas2root.exe ~/AllData/both/run00137.mid
+./midas2root.exe ~/AllData/both/run00138.mid
+./midas2root.exe ~/AllData/both/run00139.mid
+./midas2root.exe ~/AllData/both/run00140.mid
+./midas2root.exe ~/AllData/both/run00141.mid
+./midas2root.exe ~/AllData/both/run00142.mid
+./midas2root.exe ~/AllData/both/run00143.mid
+./midas2root.exe ~/AllData/both/run00144.mid
+./midas2root.exe ~/AllData/both/run00145.mid
+./midas2root.exe ~/AllData/both/run00147.mid
+./midas2root.exe ~/AllData/both/run00148.mid
+./midas2root.exe ~/AllData/both/run00151.mid
+mv ./outfiles/*.csv ~/Dropbox/Neon_MOT/Measurements/2018/12\ -\ December/Ion\ VMI/
 read varname
--- vmid.C
+++ vmid.C
@@ -1,21 +1,26 @@
-void vmid(char* runfile){
+void vmid(TString runfile){
     TFile *fin = new TFile(runfile);
-    TTree *T = (TTree*)fin->Get("v1720Data");
+    TTree *T = (TTree*)fin->Get("NeatData");
     TNtuple* ntuple = (TNtuple*)fin->Get("ntuple");
     TCanvas* c1 = new TCanvas("c1","c1",0,0,1200,800);
     // TGraph *graph = new TGraph(nselected, gsy, grep);
-    c1->Divide(3,2);
+    c1->Divide(2,3);
     c1->cd(1);
-    T->Draw("midasid");
+    T->Draw("posx:posy>>psd(100,0,1,100,0,1)","","colz");
     c1->cd(2);
-    T->Draw("timetag");
+    T->Draw("adcoverrange");
     c1->cd(3);
-    T->Draw("chan_no");
+    T->Draw("base[0]>>basehist(1000,50,350)");
     c1->cd(4);
-    T->Draw("maxadcvalue");
+    T->Draw("base[1]>>basehist1(1000,50,350)");
     c1->cd(5);
-    T->Draw("maxtimetag");
+    T->Draw("base[2]>>basehist2(1000,50,350)");
     c1->cd(6);
-    T->Draw("maxtimetag");
+    T->Draw("base[3]>>basehist3(1000,50,350)");
+    // T->Draw("maxadcvalue");
+    // c1->cd(5);
+    // T->Draw("maxtimetag");
+    // c1->cd(6);
+    // T->Draw("maxtimetag");
 // graph->Draw("ac*");
 }
